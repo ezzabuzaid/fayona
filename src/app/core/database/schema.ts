5e4dc46b1a9268bc0f44e17036f74c3a783f95ed
ezzabuzaid <ezzabuzaid@hotmail.com>
2019-06-27 01:42:58 +0300

    remove nahed work, prepare new tasks

--- src/app/core/database/schema.ts
+++ src/app/core/database/schema.ts
@@ -12,17 +12,3 @@ export class BaseSchema<T> extends MongooseSchema<T> {
         });
     }
 }
-// https://typeorm.io
-
-//     Documents
-// tslint:disable: max-line-length
-// ‘Documents’ are equivalent to records or rows of data in SQL.While a SQL row can reference data in other tables, Mongo documents usually combine that in a document.
-
-//     Fields
-// ‘Fields’ or attributes are similar to columns in a SQL table.
-
-//     Schema
-// While Mongo is schema - less, SQL defines a schema via the table definition.A Mongoose ‘schema’ is a document data structure(or shape of the document) that is enforced via the application layer.
-
-//     Models
-// ‘Models’ are higher - order constructors that take a schema and create an instance of a document equivalent to records in a relational database.
