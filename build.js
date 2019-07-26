0570c17466f882f8dda51d86cb8c816c8ab5bd8b
ezzabuzaid <ezzabuzaid@hotmail.com>
2019-07-26 20:23:20 +0300

    env error

--- build.js
+++ build.js
@@ -1,10 +1,3 @@
-const child_process = require('child_process');
-const path = require('path');
-const fs = require('fs');
-const pwd = process.cwd();
-const del = require('del');
-const { argv } = require('yargs');
-const { env } = argv;
 // function copyFile(file, directory) {
 //     const fileName = path.basename(file);
 //     const source = fs.createReadStream(file);
@@ -27,6 +20,13 @@ const { env } = argv;
 
 // TODO setup delete folder functions
 // make sure that all of them pass in pipeline
+const child_process = require('child_process');
+const path = require('path');
+const fs = require('fs');
+const pwd = process.cwd();
+const del = require('del');
+const { argv } = require('yargs');
+const { env } = argv;
 const DIST = path.join(pwd, 'dist');
 const ENV = 'environment/.env';
 const ENV_DIST = path.join(DIST, ENV);
