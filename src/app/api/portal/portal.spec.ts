391bb4270c09dc2a50c644e238f3b679eaec2b09
ezzabuzaid <ezzabuzaid@hotmail.com>
2020-02-22 19:43:20 +0200

    add more test to portal router

--- src/app/api/portal/portal.spec.ts
+++ src/app/api/portal/portal.spec.ts
@@ -1,30 +1,31 @@
 import { Constants, NetworkStatus } from '@core/helpers';
-import { UserFixture, sendRequest, generateUsername, generateExpiredToken, generateToken } from '@test/fixture';
-import { IRefreshTokenBody, ILogin } from './portal.routes';
+import { UserFixture, sendRequest, generateUsername, generateExpiredToken, generateToken, prepareUserSession, generateDeviceUUIDHeader, getUri } from '@test/fixture';
+import { IRefreshTokenBody, LoginPayload } from './portal.routes';
 import { PortalHelper } from './portal.helper';
 import * as faker from 'faker';
 import { ApplicationConstants } from '@core/constants';
 import { AppUtils } from '@core/utils';
 
 const ENDPOINT = (suffix: string) => `${Constants.Endpoints.PORTAL}/${suffix}`;
-const LOGIN_ENDPOINT = ENDPOINT(Constants.Endpoints.LOGIN);
-const RESET_ENDPOINT = ENDPOINT(Constants.Endpoints.RESET_PASSWORD);
-const FORGET_ENDPOINT = ENDPOINT(Constants.Endpoints.FORGET_PASSWORD);
+const LOGIN_ENDPOINT = getUri(ENDPOINT(Constants.Endpoints.LOGIN));
 const REFRESH_TOKEN = ENDPOINT(Constants.Endpoints.REFRESH_TOKEN);
+const RESET_ENDPOINT = getUri(ENDPOINT(Constants.Endpoints.RESET_PASSWORD));
+const FORGET_ENDPOINT = getUri(ENDPOINT(Constants.Endpoints.FORGET_PASSWORD));
 
-const fakeLoginPaylod: ILogin = {
+const fakeLoginPaylod: LoginPayload = {
     password: faker.internet.password(),
     username: generateUsername(),
 };
 
-describe('[INTERGRATION]', () => {
+describe('#INTERGRATION', () => {
     describe('Login should', () => {
         describe('fail if..', () => {
+            test('user not exist, aka username is wrong', async () => {
+                const response = await global.superAgent
+                    .post(LOGIN_ENDPOINT)
+                    .set(generateDeviceUUIDHeader())
+                    .send(fakeLoginPaylod);
 
-            test('Request with non existing user', async () => {
-                const response = await sendRequest(LOGIN_ENDPOINT, fakeLoginPaylod, {
-                    authorization: generateToken()
-                });
                 expect(response.body.message).toMatch('Wrong credintals');
                 expect(response.status).toBe(NetworkStatus.BAD_REQUEST);
             });
@@ -32,25 +33,66 @@ describe('[INTERGRATION]', () => {
             test('password is wrong', async () => {
                 const userFixture = new UserFixture();
                 await userFixture.createUser(fakeLoginPaylod);
-                const response = await sendRequest(
-                    LOGIN_ENDPOINT,
-                    AppUtils.assignObject(fakeLoginPaylod, { password: faker.internet.password() })
-                );
+                const response = await global.superAgent
+                    .post(LOGIN_ENDPOINT)
+                    .set(generateDeviceUUIDHeader())
+                    .send(AppUtils.extendObject(fakeLoginPaylod, { password: faker.internet.password() }));
+
                 expect(response.body.message).toMatch('Wrong credintals');
                 expect(response.status).toBe(NetworkStatus.BAD_REQUEST);
             });
 
-            it.todo('User has more than three session');
+            test('request headers does not have a device_uuid', async () => {
+                const response = await global.superAgent
+                    .post(LOGIN_ENDPOINT);
+                expect(response.status).toBe(NetworkStatus.UNAUTHORIZED);
+            });
+
+            test('user has more than three active session', async () => {
+                const userFixture = new UserFixture();
+                await userFixture.createUser(fakeLoginPaylod);
+
+                const session = await prepareUserSession({
+                    _id: userFixture.user.id,
+                    ...fakeLoginPaylod
+                });
+
+                await global.superAgent
+                    .post(LOGIN_ENDPOINT)
+                    .set(session.headers)
+                    .send(fakeLoginPaylod);
+
+                await global.superAgent
+                    .post(LOGIN_ENDPOINT)
+                    .set(session.headers)
+                    .send(fakeLoginPaylod);
+
+                const lastSession = await global.superAgent
+                    .post(LOGIN_ENDPOINT)
+                    .set(session.headers)
+                    .send(fakeLoginPaylod);
+
+                expect(lastSession.body.message).toMatch('exceed_allowed_sesison');
+                expect(lastSession.status).toBe(NetworkStatus.UNAUTHORIZED);
+            });
+
         });
+        describe('success if', () => {
+            test('username and password is right, request has device uuid header', async () => {
+                const userFixture = new UserFixture();
+                await userFixture.createUser(fakeLoginPaylod);
+                const response = await global.superAgent
+                    .post(LOGIN_ENDPOINT)
+                    .set(generateDeviceUUIDHeader())
+                    .send(fakeLoginPaylod);
+
+                expect(response.status).toBe(NetworkStatus.OK);
+            });
+        });
+        test.todo('returned Token is valid');
+        test.todo('returned Token has the appropriate schema');
     });
 
-    describe('Login should', () => {
-        describe('success if..', () => {
-            test.todo('if request came with valid device_uuid');
-            test.todo('if request came with non falsy username');
-            test.todo('if request came with non falsy password');
-        });
-    });
 
 });
 
@@ -91,27 +133,27 @@ describe('Refresh Token', () => {
         expect(response.status).toBe(NetworkStatus.UNAUTHORIZED);
     });
 
-    test.skip('should be UNAUTHORIZED if the device_uuid invalid', async () => {
-        const response = await getResponse(
-            generateToken(),
-            generateToken()
-        );
-        expect(response.body.message).toBe('not_authorized');
-        expect(response.status).toBe(NetworkStatus.UNAUTHORIZED);
-    });
+    // test.skip('should be UNAUTHORIZED if the device_uuid invalid', async () => {
+    //     const response = await getResponse(
+    //         generateToken(),
+    //         generateToken()
+    //     );
+    //     expect(response.body.message).toBe('not_authorized');
+    //     expect(response.status).toBe(NetworkStatus.UNAUTHORIZED);
+    // });
 
-    test.skip('should return BAD_REQUEST status and `not_allowed` if the token is not expired', async () => {
-        const response = await getResponse(
-            generateToken(),
-            generateToken(),
-            'some_device_uuid'
-        );
-        expect(response.body.message).toMatch('not_allowed');
-        expect(response.status).toBe(NetworkStatus.BAD_REQUEST);
-    });
+    // test.skip('should return BAD_REQUEST status and `not_allowed` if the token is not expired', async () => {
+    //     const response = await getResponse(
+    //         generateToken(),
+    //         generateToken(),
+    //         'some_device_uuid'
+    //     );
+    //     expect(response.body.message).toMatch('not_allowed');
+    //     expect(response.status).toBe(NetworkStatus.BAD_REQUEST);
+    // });
     test.skip('should not throw if the token is expired', async () => {
         const userUtiltiy = new UserFixture();
-        await userUtiltiy.login();
+        await prepareUserSession();
         const response = await getResponse(
             PortalHelper.generateRefreshToken(userUtiltiy.user.id),
             generateExpiredToken()
@@ -132,7 +174,3 @@ describe('Refresh Token', () => {
     // });
 });
 
-describe('Login should success when', () => {
-    it.todo('returned Token is valid');
-    it.todo('returned Token has the appropriate schema');
-});
