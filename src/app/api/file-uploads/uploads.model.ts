f3544f1652f098090a74bf9f4b18e7b6547a58d3
ezzabuzaid <ezzabuzaid@hotmail.com>
2020-03-13 16:20:43 +0200

    fix linting

--- src/app/api/file-uploads/uploads.model.ts
+++ src/app/api/file-uploads/uploads.model.ts
@@ -1,11 +1,11 @@
-import { Entity, Field, BaseModel } from "@lib/mongoose";
+import { Entity, Field, BaseModel } from '@lib/mongoose';
 
 @Entity(name)
 export class UploadsSchema {
-    @Field() type: string;
-    @Field() size: number;
-    @Field() name: string;
-    @Field() url: string;
+    @Field() public type: string;
+    @Field() public size: number;
+    @Field() public name: string;
+    @Field() public url: string;
 }
 
 export const UploadsModel = BaseModel(UploadsSchema);
