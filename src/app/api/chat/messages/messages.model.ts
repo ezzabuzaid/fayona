38d1f9fb98b13fe0f89972a84bedb0cec20524c8
ezzabuzaid <ezzabuzaid@hotmail.com>
2020-04-04 18:19:50 +0300

    rename conversation to room in message model

--- src/app/api/chat/messages/messages.model.ts
+++ src/app/api/chat/messages/messages.model.ts
@@ -1,13 +1,10 @@
-import { Entity, Field, BaseModel } from '@lib/mongoose';
+import { Entity, Field, BaseModel, PrimaryID } from '@lib/mongoose';
 import { Constants } from '@core/helpers';
-import { Types } from 'mongoose';
 
 @Entity(Constants.Schemas.MESSAGES)
 export class MessagesSchema {
     @Field({ lowercase: false }) text: string;
-    @Field() user: Types.ObjectId;
-    @Field({
-        required: true
-    }) conversation: Types.ObjectId;
+    @Field() user: PrimaryID;
+    @Field({ required: true }) room: PrimaryID;
 }
 export default BaseModel(MessagesSchema);
