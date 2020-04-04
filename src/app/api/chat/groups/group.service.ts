b92f594aa98d39c662a8d010f7c628daad99ffe0
ezzabuzaid <ezzabuzaid@hotmail.com>
2020-04-04 16:22:16 +0300

    finish add folder for the created group

--- src/app/api/chat/groups/group.service.ts
+++ src/app/api/chat/groups/group.service.ts
@@ -1,6 +1,6 @@
 import { CrudService, Repo } from '@shared/crud';
 import { groupModel, GroupsSchema } from './group.model';
-import foldersService from '@api/uploads/folders.service';
+import foldersService from '@api/uploads/folders/folders.service';
 
 export class GroupService extends CrudService<GroupsSchema> {
     // TODO: you can easily move the creation logic from the group router to be used here
@@ -9,12 +9,7 @@ export class GroupService extends CrudService<GroupsSchema> {
         super(new Repo(groupModel), {
             create: {
                 async pre(group) {
-                    const folder = await foldersService.create({
-                        name: group.id, // should be group id
-                        // FIXME there's no posiblity to assign the folder here because we need
-                        // the folder to be shared across the groups member
-                        user: group.id
-                    });
+                    const folder = await foldersService.create({ name: group.name });
                     group.folder = folder.data.id;
                 },
                 result: (group) => group
