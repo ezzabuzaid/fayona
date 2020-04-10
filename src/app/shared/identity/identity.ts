caf9355d050d039e01273a72efe00123afb702ba
ezzabuzaid <ezzabuzaid@hotmail.com>
2020-04-10 17:34:59 +0300

    fix identity was returning the decoded token

--- src/app/shared/identity/identity.ts
+++ src/app/shared/identity/identity.ts
@@ -1,8 +1,9 @@
-import { tokenService, Responses } from '@core/helpers';
+import { Responses } from '@core/helpers';
 import { Logger, AppUtils } from '@core/utils';
 import { NextFunction, Request, Response } from 'express';
 import { sessionsService } from '@api/sessions/sessions.service';
 import { ApplicationConstants } from '@core/constants';
+import { tokenService } from './token';
 export enum ERoles {
     SUPERADMIN,
     ADMIN,
@@ -15,41 +16,29 @@ class Identity {
 
     public Authorize(...roles: ERoles[]) {
         return async (req: Request, res: Response, next: NextFunction) => {
-            const decodeToken = await this.isAuthenticated(req, res, next);
-            if (roles.some((role) => role === decodeToken.role)) {
+            const { token, device_uuid } = this.extractToken(req);
+            const decodeToken = await this.verifyUserSession(token, device_uuid);
+            if (roles.includes(decodeToken.role)) {
                 next();
             }
             throw new Responses.Forbidden();
         };
     }
 
-    public async isAuthenticated(req: Request, res: Response, next: NextFunction) {
-        log.info('Start verifing JWT');
-
-        const { token, device_uuid } = this.checkParams(req);
-        try {
-            // STUB test if the request has an `authorization` header with invalid token
-            // STUB test if the request has an `authorization` header with expired token
-            const decodeToken = await tokenService.decodeToken(token);
-
-            // STUB test if there's no session associated with `deviceIdHeader` header
-            // STUB test if the session is not active
-            const session = await sessionsService.getActiveSession({ device_uuid });
-
-            if (session.hasError) {
-                // NOTE: not active mean that the session was disabled either by admin or the user logged out
-                throw new Responses.Unauthorized();
+    public isAuthenticated() {
+        return async (req: Request, res: Response, next: NextFunction) => {
+            const { token, device_uuid } = this.extractToken(req);
+            try {
+                await this.verifyUserSession(token, device_uuid);
+                next();
+            } catch (error) {
+                await sessionsService.deActivate({ device_uuid });
+                throw error;
             }
-            return decodeToken;
-        } catch (error) {
-            log.info('JWT verifing faild');
-            await sessionsService.deActivate({ device_uuid });
-            throw error;
-        }
-        next();
+        };
     }
 
-    private checkParams(req: Request) {
+    extractToken(req: Request) {
         // STUB test if the request doesn't have a `deviceIdHeader` header
         // STUB test if the request doesn't have an `authorization` header
         // TODO Amend the validate middleware to take custom response so you don't need the checking lines anymore
@@ -60,6 +49,22 @@ class Identity {
             throw new Responses.Unauthorized();
         }
         return { token, device_uuid };
+    }
+
+    async verifyUserSession(token: string, device_uuid: string) {
+        // STUB test if the request has an `authorization` header with invalid token
+        // STUB test if the request has an `authorization` header with expired token
+        const decodeToken = await tokenService.decodeToken(token);
+
+        // STUB test if there's no session associated with `deviceIdHeader` header
+        // STUB test if the session is not active
+        const session = await sessionsService.getActiveSession({ device_uuid });
+
+        if (session.hasError) {
+            // NOTE: not active mean that the session was disabled either by admin or the user logged out
+            throw new Responses.Unauthorized();
+        }
+        return decodeToken;
     }
 
 }
