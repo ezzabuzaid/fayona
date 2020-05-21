68bdf4c1f6db96debd61492964bc26ee9f509be1
ezzabuzaid <ezzabuzaid@hotmail.com>
2020-05-21 17:46:41 +0300

    add verified property to token claims

--- src/app/api/portal/portal.helper.ts
+++ src/app/api/portal/portal.helper.ts
@@ -1,14 +1,14 @@
 import { PrimaryKey } from '@lib/mongoose';
-import { tokenService, IRefreshTokenClaim, Roles } from '@shared/identity';
+import { tokenService, IRefreshTokenClaim } from '@shared/identity';
 
 export class PortalHelper {
 
-    public static generateRefreshToken(id: PrimaryKey): any {
+    public static generateRefreshToken(id: PrimaryKey) {
         return tokenService.generateToken<IRefreshTokenClaim>({ id }, { expiresIn: '12h' });
     }
 
-    public static generateToken(id: PrimaryKey, role: string) {
-        return tokenService.generateToken({ id, role }, { expiresIn: '6h' });
+    public static generateToken(id: PrimaryKey, role: string, verified: boolean) {
+        return tokenService.generateToken({ id, role, verified }, { expiresIn: '6h' });
     }
 
 }
