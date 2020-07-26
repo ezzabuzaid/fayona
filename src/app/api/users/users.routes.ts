61074cf2c38b1d2f650dbcf8a887402044b679a6
ezzabuzaid <ezzabuzaid@hotmail.com>
2020-07-26 18:22:11 +0300

    fix(UserRouter): change decorators

--- src/app/api/users/users.routes.ts
+++ src/app/api/users/users.routes.ts
@@ -1,15 +1,11 @@
 import { AccountsRouter } from '@api/profiles';
 import { Constants } from '@core/helpers';
 import { Responses, SuccessResponse } from '@core/response';
-import { cast } from '@core/utils';
-import { HttpGet, HttpPost, Route } from '@lib/restful';
-import { validate } from '@shared/common';
+import { FromBody, FromQuery, HttpGet, HttpPost, Route } from '@lib/restful';
 import { CrudRouter, Pagination } from '@shared/crud';
 import { EmailService } from '@shared/email';
 import { identity } from '@shared/identity';
 import { IsOptional, IsString } from 'class-validator';
-import { Request } from 'express';
-import { NodeServer } from '../../server';
 import { UsersSchema } from './users.model';
 import usersService, { UserService } from './users.service';
 class UsernameValidator extends Pagination {
@@ -27,27 +23,26 @@ export class UsersRouter extends CrudRouter<UsersSchema, UserService> {
         super(usersService);
     }
 
-    @HttpPost()
-    public async create(req: Request) {
-        const payload = cast<UsersSchema>(req.body);
-        const result = await this.service.create(payload);
+    @HttpPost('testCreate')
+    public async creates(@FromBody(UsersSchema) body: UsersSchema) {
+        const result = await this.service.create(body);
         if (result.hasError) {
             return new Responses.BadRequest(result.message);
         }
-        EmailService.sendVerificationEmail(NodeServer.serverUrl(req), payload.email, result.data._id);
+        EmailService.sendVerificationEmail(body.email, result.data.id);
         return new SuccessResponse(result.data, 'An e-mail has been sent to your email inbox in order to verify the account');
     }
 
-    @HttpGet(Constants.Endpoints.SEARCH, validate(UsernameValidator, 'query'))
-    public async searchForUsers(req: Request) {
-        const { username, ...options } = cast<UsernameValidator>(req.query);
+    @HttpGet(Constants.Endpoints.SEARCH)
+    public async searchForUsers(@FromQuery(UsernameValidator) query: UsernameValidator) {
+        const { username, ...options } = query;
         const users = await this.service.searchForUser(username, options);
         return new Responses.Ok(users.data);
     }
 
-    @HttpGet('username', validate(UsernameValidator, 'query'))
-    public async isUsernameExist(req: Request) {
-        const { username } = cast<UsernameValidator>(req.query);
+    @HttpGet('username')
+    public async isUsernameExist(@FromQuery(UsernameValidator) query: UsernameValidator) {
+        const { username } = query;
         const result = await this.service.one({ username });
         if (result.hasError) {
             return new Responses.BadRequest(result.message);
