f772ff6fbe7642c20b2773a256fabddc52bf9c57
ezzabuzaid <ezzabuzaid@hotmail.com>
2021-12-09 01:25:50 +0200

    build: add vscode debugger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { Logger } from './utils';
 const log = new Logger('MAIN');
 
-
 function main() {
     log.info('Node verions is => ', process.version);
     log.info('envirnoment =>', process.env.NODE_ENV);
