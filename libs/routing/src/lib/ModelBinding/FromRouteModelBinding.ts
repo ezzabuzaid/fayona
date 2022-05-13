6da345b9d042bcbba1ceddf50a9bce44cb51f7a8
ezzabuzaid <Ezzabuzaid@hotmail.com>
2022-05-13 20:11:49 +0300

    fix(Routing): convert FromRoute param value to the specified type

--- libs/routing/src/lib/ModelBinding/FromRouteModelBinding.ts
+++ libs/routing/src/lib/ModelBinding/FromRouteModelBinding.ts
@@ -1,11 +1,40 @@
+import { isNumber, isString } from 'class-validator';
+
 import { FromRoutePayloadType } from '../Metadata/FromRouteParameterMetadata';
 import { ModelBinding } from './ModelBinding';
 
+const constraintsMap = {
+  number: ['min', 'max', 'range', 'decimal', 'double', 'float', 'long'],
+  string: ['regex', 'alpha', 'maxlength', 'minlength', 'length'],
+};
+// Each of them should implement IRouteConstraint
+
 export class FromRouteModelBinding extends ModelBinding<
   FromRoutePayloadType,
-  Record<string, string>
+  Record<string, any>
 > {
-  public Bind(): any {
-    return this.Variant[this.ParameterMetadata.Payload];
+  public override Bind(): any {
+    const paramDefinition = this.ParameterMetadata.Payload;
+    const complexValidation = paramDefinition.includes(':');
+    const incomingValue = this.Variant[this.ParameterMetadata.Payload];
+    let finalValue: any = incomingValue;
+    if (complexValidation) {
+      const [paramName, paramType, ...constraints] = paramDefinition.split(':');
+      //
+    } else {
+      if (this.ParameterMetadata.ParamType === Number) {
+        finalValue = Number(incomingValue);
+        if (Number.isNaN(finalValue)) {
+          throw new Error();
+        }
+      } else if (this.ParameterMetadata.ParamType === Boolean) {
+        if (['false', 'true'].includes(incomingValue.toLowerCase())) {
+          finalValue = Boolean(incomingValue);
+        }
+        throw new Error();
+      }
+    }
+    // if validation is not configured then pass it as is
+    return finalValue;
   }
 }
