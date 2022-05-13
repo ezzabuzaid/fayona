09a3902675975edc252b6044b72ed81e3529ba10
ezzabuzaid <Ezzabuzaid@hotmail.com>
2022-05-13 20:14:32 +0300

    fix(Routing): throw validation error for body

--- libs/routing/src/lib/ModelBinding/FromBodyModelBinding.ts
+++ libs/routing/src/lib/ModelBinding/FromBodyModelBinding.ts
@@ -1,21 +1,44 @@
+import { ValidationError, validateOrReject } from 'class-validator';
+import {
+  ProblemDetails,
+  ProblemDetailsException,
+} from 'rfc-7807-problem-details';
 import { ClassType } from 'tiny-injector/Types';
 
-import {
-  FromBodyParameterMetadata,
-  FromBodyPayloadType,
-} from '../Metadata/FromBodyParameterMetadata';
+import { FromBodyPayloadType } from '../Metadata/FromBodyParameterMetadata';
 import { ModelBinding } from './ModelBinding';
 
 export class FromBodyModelBinding extends ModelBinding<
   FromBodyPayloadType,
   ClassType<any>
 > {
-  public Bind(): FromBodyPayloadType {
-    //   this.IsUserDefinedType(
-    //   this.Variant
-    // )
-    // FIXME: implement model state validation
-    return this.Variant;
+  public override async Bind(): Promise<FromBodyPayloadType> {
+    const payload = new this.ParameterMetadata.Payload();
+    Object.assign(payload, this.Variant);
+    try {
+      await validateOrReject(payload);
+    } catch (error: any) {
+      const errors = error.reduce(
+        (
+          errorMap: Record<string, string[]>,
+          validationError: ValidationError
+        ) => {
+          errorMap[validationError.property] = Object.values(
+            validationError.constraints ?? {}
+          );
+          return errorMap;
+        },
+        {}
+      );
+      const problemDetails = new ProblemDetails(
+        '400',
+        'One or more validation errors occurred.',
+        400
+      );
+      problemDetails['Errors'] = errors;
+      throw new ProblemDetailsException(problemDetails);
+    }
+    return payload;
   }
 
   private IsUserDefinedType(type: any): boolean {
