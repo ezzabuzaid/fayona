578a56213f866e6b33d36c3c5c0332b0cd6d58e6
ezzabuzaid <Ezzabuzaid@hotmail.com>
2022-05-15 17:58:13 +0300

    fix(Core): use root service collection

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -1,17 +1,7 @@
-export * from './lib/Http/HttpContext';
-export * from './lib/Middleware';
-
-export * from './lib/IWebApplication';
-export * from './lib/WebApplication';
-
-export * from './lib/IWebApplicationBuilder';
-export * from './lib/WebApplicationBuilder';
+import { ServiceType } from 'tiny-injector';
 
 export * from './lib/Exceptions/ArgumentException';
 export * from './lib/Exceptions/InvalidOperationException';
-
-export * from './lib/CoreInjector';
-export * from './lib/WebApplicationOptions';
 
 export * from './lib/Utils/Action';
 export * from './lib/Utils/Utils';
@@ -20,3 +10,12 @@ export * from './lib/Utils/SortFunctor';
 export * from './lib/Claims';
 
 export * from './lib/Metadata';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      Inject: <T>(serviceType: ServiceType<T>) => T;
+    }
+  }
+}
--- libs/core/src/lib/CoreInjector.ts
+++
@@ -1,5 +0,0 @@
-import { Injector, ServiceCollection } from 'tiny-injector';
-
-export const CORE_SERVICE_COLLECTION = new ServiceCollection();
-
-export const CoreInjector = Injector.Of(CORE_SERVICE_COLLECTION);
