ad03fbb9362522eb90f7a3235ebb69c8abfed3e7
ezzabuzaid <Ezzabuzaid@hotmail.com>
2022-05-15 18:07:03 +0300

    fix(Routing): expose full path and final handler

--- libs/core/src/lib/Metadata/HttpEndpointMetadata.ts
+++ libs/core/src/lib/Metadata/HttpEndpointMetadata.ts
@@ -6,6 +6,8 @@ import { ParameterMetadata } from './ParameterMetadata';
 export class HttpEndpointMetadata {
   public readonly Properties = new Map<any, any>();
   public readonly Parameters: ParameterMetadata<any>[] = [];
+  public FullPath?: string;
+  public FinalHandler?: RequestHandler;
 
   constructor(
     public readonly controller: Function,
