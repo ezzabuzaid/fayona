082c14e62d1fc2ad819786c296ece3bdeea68c7f
ezzabuzaid <Ezzabuzaid@hotmail.com>
2022-06-22 10:28:34 +0300

    fix(core): use TryAddScoped when adding the Context

--- libs/core/src/lib/Fayona/Fayona.ts
+++ libs/core/src/lib/Fayona/Fayona.ts
@@ -5,9 +5,7 @@ import { IFayona } from './IFayona';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 export class Fayona implements IFayona {
-  // [method: string]: any;
-
   constructor() {
-    Injector.AddScoped(Context, (context) => context);
+    Injector.TryAddScoped(Context, (context) => context);
   }
 }
